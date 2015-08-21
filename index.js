b3a3654cfe3bce3d9efdfb5f2690c2f0a8a54e22
Shivkanth B <Shiv@Shivkanths-MacBook-Pro.local>
2015-08-20 21:47:20 -0700

    complete

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 #! /usr/bin/env node
 
+var userArgs = process.argv.slice(2);
 
-console.log('gmail-open');
+
+var command = process.platform === "win32" ? "cmd" :
+                  process.platform === "darwin" ? "open" :
+                  "xdg-open";
+
+
+//https://mail.google.com/mail/u/0/#inbox
+
+if(userArgs[0])
+{
+	mailUrl= 'https://mail.google.com/mail/u/'+userArgs[0]+'/';
+}
+else
+mailUrl = 'https://mail.google.com/mail/u/0/#inbox';
+
+var exec = require('child_process').exec;
+var child = exec(command+' '+mailUrl);
+//console.log(userArgs[0]);
